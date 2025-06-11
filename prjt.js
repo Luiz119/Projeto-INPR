7f9e9e04d94576558f6e2439eb078ade17158ae4
Luiz119 <lhso2@aluno.ifal.edu.br>
2025-06-11 15:20:55 -0300

    update prjt.js

--- prjt.js
+++ prjt.js
@@ -1,1 +1,4 @@
-
+const container = document.getElementById("container");
+const div1 = document.getElementById("div1");
+const tema = document.getElementById("tema");
+const div2 = document.getElementById("div2");
